b97766f3d6970cdcd027231ccdb9142ec7c2391f
LuizGabrielSS <gabriel.samuel@outlook>
2022-02-27 17:05:54 -0300

    pronto1.0

--- fala.py
+++ fala.py
@@ -8,33 +8,40 @@ fala = choice(falas)
 perturba = choice(perturbar)
 erro = choice(erros)
 
+resposta = input("Gostaria de perturbar alguem,imitar ou so falar?:")
+
 def sai_som(resposta):
 	reproducao.say(resposta)
 	reproducao.runAndWait()
+def brincar():
+    rec = sr.Recognizer()
 
-rec = sr.Recognizer()
+    with sr.Microphone() as s:
+        rec.adjust_for_ambient_noise(s)
+        try:
+            while True:
 
-with sr.Microphone() as s:
-    rec.adjust_for_ambient_noise(s)
-while True:
-    audio = rec.listen(s)
-    texto = rec.recognize_google(audio,language = "pt")
+                audio = rec.listen(s)
+                texto = rec.recognize_google(audio,language = "pt")
 
-    resposta = input("Gostaria de perturbar alguem,imitar ou so falar?:")
-    #resposta = 
+                #resposta = 
 
-    if resposta == 'imitar':
-        sai_som(texto)
-        print(texto)
+                if resposta == 'imitar':
+                    sai_som(texto)
+                    print(texto)
 
-    elif resposta == 'falar':
-        sai_som(fala)
-        print(fala)
+                elif resposta == 'falar':
+                    sai_som(fala)
+                    print(fala)
 
-    elif 'perturbar' in resposta:
-        sai_som(perturba)
-        print(perturba)
-    
-    else:
-        sai_som(erro)
-        print(erro)
+                elif 'perturbar' in resposta:
+                    sai_som(perturba)
+                    print(perturba)
+
+        except sr.UnknownValueError:
+                    sai_som(erro)
+
+if __name__ == '__main__':
+    sai_som("iniciando")
+    brincar()
+
